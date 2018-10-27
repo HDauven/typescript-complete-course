ec3087e92bce9bae9f3e6254d8fecfb80918f00a
Hein Dauven <heindauven@gmail.com>
2018-10-27 19:11:15 +0200

    Namespaces in TypeScript

---
+++ section-6/Math.ts
@@ -0,0 +1,6 @@
+// Namespace imports, displaying namespacing in TypeScript
+/// <reference path="circleMath.ts" />
+/// <reference path="rectangleMath.ts" />
+
+console.log(MyMath.calculateRectangle(10, 20));
+console.log(MyMath.calculateCircumference(3));
