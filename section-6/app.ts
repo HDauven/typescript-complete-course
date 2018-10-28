ebbf6f327e2451194b712c07ab3b5ea69e51464d
Hein Dauven <heindauven@gmail.com>
2018-10-28 23:03:26 +0100

    Import math functions

---
+++ section-6/app.ts
@@ -0,0 +1,12 @@
+// /// <reference path="circleMath.ts" />
+// /// <reference path="rectangleMath.ts" />
+
+// console.log(MyMath.calculateRectangle(10, 20));
+// console.log(MyMath.calculateCircumference(3));
+
+import { PI, calculateCircumference } from "./math/circle";
+import { calculateRectangle } from "./math/rectangle";
+
+console.log(PI);
+console.log(calculateCircumference(10));
+console.log(calculateRectangle(3, 13));
